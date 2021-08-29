faedd5ec2da6d75639d71341e47462b5f9c48ab7
Mohit Sharma <imoisharma@icloud.com>
2021-08-29 21:58:15 +1000

    provided the iam policy

--- iam/main.tf
+++ iam/main.tf
@@ -1,20 +1,18 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.56"
-    }
+resource "aws_iam_policy" "policy" 
+  {  
+    name = "${random_pet.pet_name.id}-policy"  
+    description = "My test policy"
+    policy = <<EOT
+  {  "Version": "2012-10-17",  
+     "Statement": [    
+       {  "Action": ["s3:ListAllMyBuckets"],
+          "Effect": "Allow",
+          "Resource": "*"   
+       },
+       {  "Action": ["s3:*"],      
+          "Effect": "Allow",
+          "Resource": "${aws_s3_bucket.bucket.arn}"
+       }]
   }
-}
-
-# Configure the AWS Provider
-provider "aws" {
-    profile= "default"
-    region = "us-east-1"
-    
-}
-
-# Create a VPC
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
-}
+  EOT
+  }
